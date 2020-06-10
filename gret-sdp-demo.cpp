809c82262553d3d12f2c0cebf685142abd9ccf4f
Felix Herrmann <felix.rodau@gmail.com>
2020-06-10 09:57:04 +0200

    update .gitignore

--- gret-sdp-demo.cpp
+++ gret-sdp-demo.cpp
@@ -6,11 +6,11 @@
 #include <sdpa_call.h>
 #include "shared.h"
 
-static const int n = 10;
+static const int n = 1000;
 static const int d = 3;
 static const int m = 5;
-static const float samp = 0.7; // sample probability
-using Scalar = gr::Point3D<float>::Scalar;
+static const double samp = 0.7; // sample probability
+using Scalar = gr::Point3D<double>::Scalar;
 using MatrixType = Eigen::Matrix<Scalar, d+1, d+1>;
 using VectorType = Eigen::Matrix<Scalar, d, 1>;
 using RigidTransformation = std::tuple<MatrixType, VectorType>;
@@ -80,9 +80,9 @@ template <typename InRange, typename TrRange>
 void GeneratePatches(const InRange& pointCloud, TrRange& transformations,
                       Eigen::Ref<MatrixX> L, Eigen::Ref<MatrixX> B, Eigen::Ref<MatrixX> D){
 
-  L = Eigen::MatrixXf::Zero(n+m,n+m);
-  B = Eigen::MatrixXf::Zero(m*d, n+m);
-  D = Eigen::MatrixXf::Zero(m*d, m*d);
+  L = Eigen::MatrixXd::Zero(n+m,n+m);
+  B = Eigen::MatrixXd::Zero(m*d, n+m);
+  D = Eigen::MatrixXd::Zero(m*d, m*d);
 
 
   GenerateTrafos(transformations);
@@ -93,7 +93,7 @@ void GeneratePatches(const InRange& pointCloud, TrRange& transformations,
   for(int k = 0; k < n; k++){
     // for every patch i
     for(int i = 0; i < m; i++){
-      if(((float) rand() / (RAND_MAX)) < samp){
+      if(((double) rand() / (RAND_MAX)) < samp){
         v_ki = transformations.at(i).block(0,0,d,d) * pointCloud.at(k).pos() + transformations.at(i).block(0,d,d,1);
 
         L(k,k)++;
@@ -139,7 +139,7 @@ void SolveSDP(Eigen::Ref<const MatrixX> C, Eigen::Ref<MatrixX> G){
   SDPA	Problem;
 
   // All parameteres are renewed
-  Problem.setParameterType(SDPA::PARAMETER_DEFAULT);
+  Problem.setParameterType(SDPA::PARAMETER_STABLE_BUT_SLOW);
 
   Problem.printParameters(stdout);
 
@@ -152,17 +152,15 @@ void SolveSDP(Eigen::Ref<const MatrixX> C, Eigen::Ref<MatrixX> G){
 
   Problem.initializeUpperTriangleSpace();
 
-
   // c vec
   int cnt = 1;
   for(int i = 0; i < m; i++){
     for(int j = 0; j < d; j++){
       Problem.inputCVec(cnt++,1);
-      
       for(int k = j+1; k < d; k++)
         Problem.inputCVec(cnt++,0);
     }
-  }
+  } 
 
   // F0 = -C
   for(int i = 0; i < m*d; i++)
@@ -172,9 +170,10 @@ void SolveSDP(Eigen::Ref<const MatrixX> C, Eigen::Ref<MatrixX> G){
  // Fi
   cnt = 1;
   for(int k = 0; k < m; k++){
-    for(int j = 0; j < d; j++){
-      for(int i = j; i < d; i++)
+    for(int i = 0; i < d; i++){
+      for(int j = i; j < d; j++){
         Problem.inputElement(cnt++, 1, k*d+i+1, k*d+j+1, 1);
+      }
     }
   }
 
@@ -243,8 +242,53 @@ void SolveSDP(Eigen::Ref<const MatrixX> C, Eigen::Ref<MatrixX> G){
           stdout);
     }
   }
+
+  double* yMat = Problem.getResultYMat(1);
+  Eigen::Map<Eigen::MatrixXd> Gmap(yMat, d*m, d*m);
+  G = Gmap;
 }
 
+
+template <typename TrRange>
+void ComputeRelativeTrafos(const TrRange& transformations, TrRange& relTransformations, bool inv = false){
+  relTransformations.reserve(m-1);
+  MatrixType Tr;
+  Eigen::MatrixXd O1(transformations.at(0).block(0, 0, d, d));
+  if(!inv)
+    O1 = O1.inverse();
+  Eigen::VectorXd t1(transformations.at(0).block(0, d, d, 1));
+  Eigen::MatrixXd O(d,d);
+  Eigen::VectorXd t(d);
+
+  for(int i = 1; i < m; i++){
+    Tr = transformations.at(i);
+    O = Tr.block(0, 0, d, d);
+    if(inv) O = O.inverse();
+    t = Tr.block(0, d, d, 1);
+    Tr.block(0, 0, d, d) = O * O1;
+    Tr.block(0, d, d, 1) = (t-t1)*O1;
+    relTransformations.push_back(Tr);
+  }
+}
+
+void writeMatrix(Eigen::Ref<Eigen::MatrixXd> m, std::string filename){
+  std::ofstream file("/home/felix/MATLAB/Projects/SDP/" + filename);
+  if(file.is_open()){
+    file << m << '\n';
+  }
+  file.close();
+}
+
+template <typename PointRange>
+void writePoints(const PointRange& vec, std::string filename){
+  std::ofstream file(filename);
+  if(file.is_open()){
+    for(auto& p : vec){
+      file << p.pos().transpose() << std::endl;
+    }
+  }
+  file.close();
+}
 
 int main ()
 {
@@ -252,6 +296,7 @@ int main ()
   vector<gr::Point3D<Scalar>> spiral;
   spiral.reserve(n);
   CreateSpiral(spiral, n);
+  writePoints(spiral, "spiral.dat");
 
   vector<MatrixType> originalTransformations;
   originalTransformations.reserve(m);
@@ -268,13 +313,90 @@ int main ()
   // compute C
   C = D - B * Linv * B.transpose();
 
-  // TODO: solve the SDP (P2) using C
+  writeMatrix(L, "LMat.dat");
+  writeMatrix(B, "BMat.dat");
+  writeMatrix(Linv, "LinvMat.dat");
+  writeMatrix(D, "DMat.dat");
+  writeMatrix(C, "CMat.dat");
+
+  // solve the SDP (P2) using C
   Eigen::Matrix<Scalar, m*d, m*d> G;
 
   SolveSDP(C, G);
 
-}
+  writeMatrix(G, "GMat.dat");
 
+  // compute top d eigenvalues and eigenvectors
+  Eigen::EigenSolver<Eigen::Matrix<Scalar, m*d, m*d>> s(G);
+  Eigen::VectorXcd eigvals = s.eigenvalues();
+  Eigen::MatrixXcd eigvecs = s.eigenvectors();
+  std::vector<double> re_eigvals;
+  std::vector<Eigen::VectorXd> re_eigvecs;
+ 
+  for(int i = 0; i < eigvals.rows(); i++) {
+      if(eigvals(i).imag() == 0){
+          re_eigvals.push_back(eigvals(i).real());
+          re_eigvecs.push_back(eigvecs.col(i).real());
+      }
+  }   
+
+  std::vector<std::pair<double, Eigen::VectorXd>> eig_pairs;
+  eig_pairs.reserve(d);
+  std::transform(re_eigvals.begin(), re_eigvals.end(), re_eigvecs.begin(), std::back_inserter(eig_pairs),
+               [](double a, Eigen::VectorXd b) { return std::make_pair(a, b); });
+
+  sort(eig_pairs.begin(), eig_pairs.end(),
+    [&](std::pair<double, Eigen::VectorXd>& a, std::pair<double, Eigen::VectorXd>& b) {
+        return (a.first > b.first);
+    }
+  );
+
+
+  // construct W
+  Eigen::Matrix<Scalar, d, m*d> W;
+  for(int i = 0; i < d; i++){
+      W.row(i) = sqrt(eig_pairs[i].first) * eig_pairs[i].second.transpose();
+  }
+
+  // compute transformations O
+  Eigen::Matrix<Scalar, d, m*d> O;
+  for(int i = 0; i < m; i++){
+      Eigen::Ref<Eigen::Matrix<Scalar, d, d>> w(W.block(0, i*d, d, d));
+      Eigen::JacobiSVD<Eigen::Matrix<Scalar, d, d>> svd(w, Eigen::ComputeFullU | Eigen::ComputeFullV);
+      O.block<d, d>(0, i*d) = svd.matrixU() * svd.matrixV().transpose();
+  }
+
+  // compute Z
+  Eigen::Matrix<Scalar, d, n+m> Z;
+  Z = O*B*Linv;
+
+  writeMatrix(Z, "ZMat.dat");
+  writeMatrix(W, "WMat.dat");
+  writeMatrix(O, "OMat.dat");
+
+  // compute trafos
+  std::vector<MatrixType> transformations;
+  MatrixType trafo;
+  transformations.reserve(m);
+  for(int i = 0; i < m; i++){
+    trafo = MatrixType::Zero();
+    trafo.block(0, 0, d, d) = O.block(0, i*d, d, d);
+    trafo.block(0, d, d, 1) = Z.block(0, n+i, d, 1);
+    trafo(d,d) = 1;
+    transformations.push_back(trafo);
+  }
+
+  std::vector<MatrixType> relTransformations;
+  ComputeRelativeTrafos(transformations, relTransformations);
+
+  std::vector<MatrixType> origRelTransformations;
+  ComputeRelativeTrafos(originalTransformations, origRelTransformations, true);
+
+  for(int i = 0; i < m-1; i++){
+    std::cout << "Tr*[" << i << "] " << endl << relTransformations.at(i) << endl;
+    std::cout << "Tr**[" << i << "] " << endl << origRelTransformations.at(i) << endl;
+  }
+}
 
 
 
